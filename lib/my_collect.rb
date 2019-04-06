249331cb1ff892631e8a1298855552645222c07a
lesliesage <leslie.sage@gmail.com>
2019-04-06 11:27:27 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(array)
+  if block_given?
+    i = 0
+    mod = []
+    while i < array.length
+      mod << yield(array[i])
+      i += 1
+    end
+    mod
+  else nil
+  end
+end
 
-
+# my test
+# my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) {
+#   |i| puts i.split(" ").first
+# }
